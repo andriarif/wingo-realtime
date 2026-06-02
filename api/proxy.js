15fbd362d3286f2302b606f0770965cc9c263981
andriarif <55911915+andriarif@users.noreply.github.com>
2026-06-02 13:53:36 +0700

    Refactor fetch request handling in proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,34 +1,36 @@
 export default async function handler(req, res) {
 
-  try {
+    try {
 
-    const response = await fetch(
-      'https://spin88.sbs/api/webapi/GetNoaverageEmerdList',
-      {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
-          typeid: '1',
-          pageno: '0',
-          pageto: '10',
-          language: 'vi'
-        })
-      }
-    );
+        const response = await fetch(
+            'https://spin88.sbs/api/webapi/GetNoaverageEmerdList',
+            {
+                method: 'POST',
 
-    const data = await response.json();
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
 
-    res.status(200).json(data);
+                body: new URLSearchParams({
+                    typeid: '1',
+                    pageno: '0',
+                    pageto: '10',
+                    language: 'vi'
+                })
+            }
+        );
 
-  } catch (error) {
+        const data = await response.json();
 
-    res.status(500).json({
-      error: error.message
-    });
+        res.status(200).json(data);
 
-  }
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+
+    }
 
 }
