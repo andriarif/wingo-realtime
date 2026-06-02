7f401b13ebc09f61e3eb7ebbd76d6aec990e9b1e
andriarif <55911915+andriarif@users.noreply.github.com>
2026-06-02 13:43:22 +0700

    Add API proxy handler for external data fetching

---
+++ api/proxy.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+
+  try {
+
+    const response = await fetch(
+      'https://spin88.sbs/api/webapi/GetNoaverageEmerdList',
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          typeid: '1',
+          pageno: '0',
+          pageto: '10',
+          language: 'vi'
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
+}
