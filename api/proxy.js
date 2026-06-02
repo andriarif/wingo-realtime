8b83f80b80613acce115eb1a6f2b4283e4af4bb1
andriarif <55911915+andriarif@users.noreply.github.com>
2026-06-02 14:02:01 +0700

    Update request headers and body format in proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,20 +5,16 @@ export default async function handler(req, res) {
         {
             method: 'POST',
             headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/x-www-form-urlencoded'
+                'Accept': '*/*',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': 'Mozilla/5.0'
             },
-            body: new URLSearchParams({
-                typeid: '1',
-                pageno: '0',
-                pageto: '10',
-                language: 'vi'
-            })
+            body: 'typeid=1&pageno=0&pageto=10&language=vi'
         }
     );
 
     const text = await response.text();
 
+    res.setHeader('Content-Type', 'text/plain');
     res.status(200).send(text);
-
 }
